c9f429af5b5616aaf9ad8076bd42f54f94a4c50a
LifeFile <lifefiledev@gmail.com>
2026-08-15 20:40:36 -0500

    US-1.1: project skeleton with provider architecture folders

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  GOOGLE_CLIENT_ID: "PASTE-YOUR-CLIENT-ID-HERE.apps.googleusercontent.com",
+  SCOPES: "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/calendar.events",
+};
